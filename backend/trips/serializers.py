3ee5a6312fc71f783eb55e56407a9b33bd56af24
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 12:25:29 +0300

    Updated serializers.py file

--- backend/trips/serializers.py
+++ backend/trips/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Trip, Log
+from .models import Driver
 
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,22 @@ class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
         fields = '__all__'
+        
+class DriverRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = Driver
+        fields = ('username', 'password', 'email', 'license_number', 'company', 'phone')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = Driver.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            license_number=validated_data['license_number'],
+            company=validated_data.get('company', ''),
+            phone=validated_data['phone']
+        )
+        return user
