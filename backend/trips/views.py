63d8ea1fceee0f077a10bc8018d99303461838a0
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 11:44:42 +0300

    Updated views.py file

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Trip, Log
+from .serializers import TripSerializer, LogSerializer
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+class LogViewSet(viewsets.ModelViewSet):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
 
 # Create your views here.
