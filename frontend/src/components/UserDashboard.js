93d8e66184f4dbd7e9f81923957c0d02972a85b6
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 11:18:09 +0300

    User dashboard created

---
+++ frontend/src/components/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserDashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Driver Dashboard</h1>
+            {/* Add trip management components */}
+        </div>
+    );
+};
+
+export default UserDashboard;
