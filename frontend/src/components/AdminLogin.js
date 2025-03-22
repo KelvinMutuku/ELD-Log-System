c22f86a11dae3ec23cbdd3f26e72e9b941ba63d1
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 11:14:05 +0300

    Admin login page created

---
+++ frontend/src/components/AdminLogin.js
@@ -0,0 +1,17 @@
+// src/components/AdminLogin.js
+import React from 'react';
+
+const AdminLogin = () => {
+  return (
+    <div className="login-container">
+      <h2>Admin Login</h2>
+      <form className="login-form">
+        <input type="text" placeholder="Admin ID" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminLogin;
