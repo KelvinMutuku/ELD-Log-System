7775467e45e22472cf1e25daf86fbfe7044c567d
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 11:10:15 +0300

    Home page created

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to ELD Log System</h1>
+      <p>Please select your login type from the navigation bar</p>
+    </div>
+  );
+};
+
+export default Home;
