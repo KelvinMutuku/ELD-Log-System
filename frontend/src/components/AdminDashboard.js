cdd686e863e3356b963f3279397f7a467addbb47
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 11:15:21 +0300

    Admin dashboard created

---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Admin Dashboard</h1>
+            {/* Add admin management components */}
+        </div>
+    );
+};
+
+export default AdminDashboard;
