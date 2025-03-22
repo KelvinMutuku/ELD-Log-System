5df64ce228284c781925f264076738d1e1f158d0
Kelvin Mutuku <mutukuk553@gmail.com>
2025-03-22 12:06:08 +0300

    urls.py and views.py file updated

--- backend/trips/urls.py
+++ backend/trips/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TripViewSet, LogViewSet
+from .views import TripViewSet, LogViewSet, UserLogin
 
 router = DefaultRouter()
 router.register(r'trips', TripViewSet)
@@ -8,4 +8,5 @@ router.register(r'logs', LogViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', UserLogin.as_view(), name='user_login'),
 ]
--- backend/trips/views.py
+++ backend/trips/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
 from .models import Trip, Log
 from .serializers import TripSerializer, LogSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all().order_by('-created_at')
@@ -9,4 +14,19 @@ class TripViewSet(viewsets.ModelViewSet):
 class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all().order_by('-date')
     serializer_class = LogSerializer
-    
+
+
+class UserLogin(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+                'is_admin': user.is_staff
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
